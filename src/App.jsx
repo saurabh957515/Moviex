caf9ec6ea93891fb0636d76640e32bb2ccb7a88e
saurabh <rathvasaurabhkumar@gmail.com>
2023-05-20 17:43:03 +0530

    other files delted

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,7 @@ import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import Details from "./pages/details/Details";
-import SearchResult from "./pages/searchResult/SearchResult";
-import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/PageNotFound";
+
 
 function App() {
   const dispatch = useDispatch()
@@ -19,7 +16,7 @@ function App() {
     fetchApiConfig();
     genresCall()
 
-   },[]);
+   },[url]);
 
    const fetchApiConfig = () => {
     fetchDataFromApi("/configuration").then((res)=>{
